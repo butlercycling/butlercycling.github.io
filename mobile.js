c1934e36a370204f5e0e41ce10edfe98aba5870e
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-11-14 18:22:53 -0700

    added handling for cases of both resizing and inital mobile sizing

--- mobile.js
+++ mobile.js
@@ -1,29 +1,37 @@
 $(document).ready(function(){
 
+        var menutog = function () {
+            if ($("#menu-toggle").hasClass("active")){
+                $(".nav-links").css("visibility", "hidden");
+            }
+            else {
+                $(".nav-links").css("visibility", "visible");
+            }
+            $("#menu-toggle").toggleClass("active");
 
+        } 
         if ($(document).width() < 900) {
             $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
-        }
-        $('#menu-toggle').on('click', function() {
-                        if ($("#menu-toggle").hasClass("active")){
-                            $(".nav-links").css("visibility", "hidden");
-                        }
-                        else {
-                            $(".nav-links").css("visibility", "visible");
-                        }
-                        $("#menu-toggle").toggleClass("active");
-
-        });
+            $('#menu-toggle').on('click', menutog);
+        
         $('nav a').on('click', function() {
             $("#menu-toggle").removeClass("active");
             $(".nav-links").css("visibility", "hidden");
-       });
+        });
+        }
         $(window).resize(function() {
             if ($(document).width() < 900 && !$('#menu-toggle').length) {
                 $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
+                $('#menu-toggle').on('click', menutog);
+        
+                $('nav a').on('click', function() {
+                    $("#menu-toggle").removeClass("active");
+                    $(".nav-links").css("visibility", "hidden");
+                });
             }
             else if ($(document).width() > 900) {
-                $('#menu-toggle').remove()
+                $('#menu-toggle').remove();
+                $(".nav-links").css("visibility", "");
             }
         });
 });
