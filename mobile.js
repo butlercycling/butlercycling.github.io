de67de3fec051b96fe1da4fa2dcdd0e16c1adc38
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-11-17 11:33:51 -0700

    fixed extraneous JS that wasn't removed during menu-toggle refactoring. Menu should no longer disappear occasionally.

--- mobile.js
+++ mobile.js
@@ -13,21 +13,11 @@ $(document).ready(function(){
         if ($(document).width() < 900) {
             $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
             $('#menu-toggle').on('click', menutog);
-        
-        $('nav a').on('click', function() {
-            $("#menu-toggle").removeClass("active");
-            $(".nav-links").css("visibility", "hidden");
-        });
         }
         $(window).resize(function() {
             if ($(document).width() < 900 && !$('#menu-toggle').length) {
                 $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
                 $('#menu-toggle').on('click', menutog);
-        
-                $('nav a').on('click', function() {
-                    $("#menu-toggle").removeClass("active");
-                    $(".nav-links").css("visibility", "hidden");
-                });
             }
             else if ($(document).width() > 900) {
                 $('#menu-toggle').remove();
