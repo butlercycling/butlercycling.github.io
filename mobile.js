7590cfdd1bbec73e69382eb4aa74f7e23dbe76a8
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-11-18 14:10:59 -0700

    fixed logo accidentally triggering menu in mobile

--- mobile.js
+++ mobile.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
             if ($(document).width() <= 900 && !$('#menu-toggle').length) {
                 $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
                 $('#menu-toggle').on('click', toggle_nav);
-                $('nav a').on('click', toggle_nav);
+                $('nav ul a').on('click', toggle_nav);
             }
             else if ($(document).width() > 900) {
                 $('#menu-toggle').remove();
-                $('nav a').off('click', toggle_nav);
+                $('nav ul a').off('click', toggle_nav);
                 $(".nav-links").css("visibility", "");
             }
         }
