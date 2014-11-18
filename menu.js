04ae8c6f98510029b9dc7ec7a4a9cc0e00fa4dad
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-11-18 13:51:37 -0700

    changed order of UI munging to help hide extended response time on server read.

--- menu.js
+++ menu.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('#roster-left li').on('click', function() {
                                           $('.selected').removeClass('selected');
                                           $(this).addClass('selected');
-                                          $("#roster-right h3").html($(this).html());
                                           var fname = $(this).html().toLowerCase().replace(/ /g, '');
                                           console.log(fname);
                                           $.ajax( "data/"+fname+".txt" )
@@ -13,6 +12,6 @@ $(document).ready(function(){
                                                         $("#roster-right .bio p").html("");
                                                         console.log("error: couldn't load "+fname+".txt");
                                                     });
-                                        
+                                          $("#roster-right h3").html($(this).html()); 
     });
 });
