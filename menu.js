2ab4e9863704ec6518bff9996490dd98551b763d
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-11-17 17:08:22 -0700

    took out no bio placeholder

--- menu.js
+++ menu.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
                                           $('.selected').removeClass('selected');
                                           $(this).addClass('selected');
                                           $("#roster-right h3").html($(this).html());
-                                          $("#roster-right .bio span").html("No Bio");
                                           var fname = $(this).html().toLowerCase().replace(/ /g, '');
                                           console.log(fname);
                                           $.ajax( "data/"+fname+".txt" )
