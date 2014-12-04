e94f842012ddc8a2ddd4a64d6b70b4fb2a84aff0
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2014-12-04 09:34:35 -0700

    fixed bad code for team pages

--- menu.js
+++ menu.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
                                           var fname = $(this).children('span').html().toLowerCase().replace(/ /g, '');
                                           $.ajax( "data/"+fname+".txt" )
                                                     .success(function(data) {
-                                                        $("#roster-right .bio table").html(data);
+                                                        $("#roster-right .bio #details").html(data);
                                                     })
                                                     .fail(function() {
-                                                        $("#roster-right .bio table").html("");
+                                                        $("#roster-right .bio #details").html("");
                                                         console.log("error: couldn't load "+fname+".txt");
                                                     })
                                                     .always(function() {
