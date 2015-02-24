2fff06c57893aef86b0172f833bf487f2f357db0
Nate Prewitt <Nate.Prewitt@Colorado.edu>
2015-02-24 13:53:00 -0700

    rolling over new site

--- mobile.js
+++ mobile.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
         var toggle_nav = function () {
-            $(".nav-links").toggleClass("active");
+            $("#mainnav ul").toggleClass("active");
         }
         var init_mobile_nav = function () {
             if ($(document).width() <= 900 && !$('#menu-toggle').length) {
-                $(".nav-links").before('<i id="menu-toggle" class="fa fa-bars"></i>');
+                $("#mainnav ul").before('<i id="menu-toggle" class="fa fa-bars"></i>');
                 $('#menu-toggle').on('click', toggle_nav);
                 $('nav ul a').on('click', toggle_nav);
             }
             else if ($(document).width() > 900) {
                 $('#menu-toggle').remove();
                 $('nav ul a').off('click', toggle_nav);
-                $(".nav-links").css("visibility", "");
+                $("#mainnav ul").css("visibility", "");
             }
         }
         init_mobile_nav();
